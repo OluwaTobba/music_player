4b52d741ec2f48effbe073d184e6669754f341e5
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-09-22 18:04:13 +0100

    Updated data.js

--- data.js
+++ data.js
@@ -11,4 +11,35 @@ let songs = [
         artist: 'Dunsin Oyekan',
         cover: 'assets/images/cover 13.png'
     },
+    {
+        name: 'Worthy of my praise',
+        path: 'assets/musics/Worthy of My Praise.mp3',
+        artist: 'Dunsin Oyekan ft. Lawrence Oyor',
+        cover: 'assets/images/cover 13.png'
+    },
+    {
+        name: 'Do To Me What You Want',
+        path: 'assets/musics/Dunsin Oyekan - Do To Me What You Want.mp3',
+        artist: 'Dunsin Oyekan',
+        cover: 'assets/images/cover 13.png'
+    },
+    {
+        name: 'Yahweh',
+        path: 'assets/musics/Labisi - Yahweh.mp3',
+        artist: 'Labisi',
+        cover: 'assets/images/cover 13.png'
+    },
+    
+    {
+        name: 'Dance Anyhow',
+        path: 'assets/musics/Dance Anyhow.mp3',
+        artist: 'Judikay',
+        cover: 'assets/images/cover 13.png'
+    },
+    {
+        name: 'Nathaniel Bassey @ 82Hours MMP',
+        path: 'assets/musics/Nathaniel Bassey - 82Hours MMP.m4a',
+        artist: 'Nathaniel Bassey',
+        cover: 'assets/images/cover 13.png'
+    }
 ];
